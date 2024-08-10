9bfb4b092a54832b927fc13a9f2a2a9a841c9d2e
Karen Solange Dutik <karensolangedutik@Karens-MacBook-Pro.local>
2024-08-09 21:09:40 -0300

    heroku

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use = (request, response, next) => {
   console.log(request.body);
   next();
 };
+
 let notes = [
   {
     id: 1,
@@ -91,7 +92,8 @@ app.use = (request, response) => {
     error: "Not found",
   });
 };
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
